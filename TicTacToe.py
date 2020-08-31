2df2c863e68b7dffb7f015720b6d3cbc9c1037a0
Nishma <nismaluitel@gmail.com>
2020-08-31 15:51:15 -0500

    Displaying and interaction between two players and win check executed

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,72 +1,51 @@
-#displays the information for the user
-def display(row1,row2,row3):
-    print(row1)
-    print(row2)
-    print(row3)
+from IPython.display import clear_output
 
-row1 = [' ',' ',' ']
-row2 = [' ',' ',' ']
-row3 = [' ',' ',' ']
+def display_board(board):
+    print('   |   |  ')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' ')
+    print('-----------')
+    print('   |   |  ')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' ')
+    print('-----------')
+    print('   |   |  ')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' ')
 
-#User's choice for which row and column they want to edit
-def user_choice():
+def player_input():
+    marker = ''
 
-    choice = 'Wrong'
-    input_value = 'Wrong'
-    acceptable_row = [1,2,3]
-    acceptable_column = [0,1,2] 
-    i = 0
-    #for i in range(0,9): 
-    while choice.isdigit() == False and input_value.isdigit() == False:
-        choice = input('Please enter the row you want to select (1-3): ')
-        input_value = input('Please enter the column (0-2): ')
+    #Keep asking Player 1 to choose X or O
+    while marker != 'X' and marker != 'O':
+        marker = input('Player1, choose X or O: ')
 
-        if choice.isdigit() == False:
-            print("Sorry that is not a digit")
+    #Assign Player 2, the opposite marker
+    player1 = marker
 
-        if choice in acceptable_row:
-            print("Sorry, the value must be 1,2,3 ")
+    if player1 == 'X':
+        player2 = 'O'
 
-        if input_value.isdigit() == False:
-            print("Sorry that is not a digit")
-
-        if input_value in acceptable_column:
-            print("Sorry, the value must be 0,1,2 ")
-
-    if choice == '1':
-        if input_value == '0':
-            row1[0] = 'X'
-
-        elif input_value == '1':
-            row1[1] = 'X'
-
-        elif input_value == '2':
-            row1[2] = 'X'
-
-    if choice == '2':
-        if input_value == '0':
-            row2[0] = 'X'
-
-        elif input_value == '1':
-            row2[1] = 'X'
-
-        elif input_value == '2':
-            row2[2] = 'X'
-
-    if choice == '3':
-        if input_value == '0':
-            row3[0] = 'X'
-
-        elif input_value == '1':
-            row3[1] = 'X'
-
-        elif input_value == '2':
-            row3[2] = 'X'
-
-    display(row1,row2,row3)
-
-display(row1,row2,row3)
-user_choice()
+    else:
+        player2 = 'X'
+    return (player1,player2)
 
 
+def place_marker(board, marker, position):
+    board[position] = marker
 
+def win_check(board,mark):
+    return(board[7] == mark and board[8] == mark and board[9] == mark or #across the top
+        board[4] == mark and board[5] == mark and board[6] == mark or #across the middle
+        board[1] == mark and board[2] == mark and board[3] == mark or #across the bottom
+        board[1] == mark and board[4] == mark and board[7] == mark or #up the left side
+        board[2] == mark and board[5] == mark and board[8] == mark or #up the middle
+        board[3] == mark and board[6] == mark and board[9] == mark or #up the right
+        board[1] == mark and board[5] == mark and board[9] == mark or #bottom to up right diagonal
+        board[3] == mark and board[5] == mark and board[7] == mark) #bottom to up left diagonal
+
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+#test_board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+#display_board(test_board)
+player1_variable, player2_variable = player_input()
+
+place_marker(test_board,'$',8)
+display_board(test_board)
+win_check(test_board,'X')
